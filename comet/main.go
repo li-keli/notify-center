e3b532f124a384c2cd35cf6746e83b560c6c1e4d
李科笠 <13120168539@163.com>
2019-07-09 09:47:40 +0800

    实现websocket

--- comet/main.go
+++ comet/main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"github.com/emirpasic/gods/maps/hashmap"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+var (
+	conns   = hashmap.New()
+	upgrade = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
 
 func main() {
-	fmt.Print("ok")
+
+	engine := gin.Default()
+
+	engine.GET("/health/:jsjUniqueId", func(c *gin.Context) {
+		c.String(http.StatusOK, string(conns.Size()))
+		if v, isfound := conns.Get(c.Param("jsjUniqueId")); isfound {
+			conn := v.(*websocket.Conn)
+			_ = conn.WriteMessage(1, []byte("hello"))
+		}
+	})
+
+	engine.GET("/v1/ws/:targetType/:jsjUniqueId", func(c *gin.Context) {
+		var (
+			jsjUniqueId = c.Param("jsjUniqueId")
+		)
+
+		ws, err := upgrade.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			return
+		}
+
+		conns.Put(jsjUniqueId, ws)
+		defer func() {
+			conns.Remove(jsjUniqueId)
+			ws.Close()
+		}()
+
+		for {
+			//读取ws中的数据
+			mt, message, err := ws.ReadMessage()
+			if err != nil {
+				break
+			}
+
+			if string(message) == "+" {
+				message = []byte("-")
+			}
+			//写入ws数据
+			err = ws.WriteMessage(mt, message)
+			if err != nil {
+				break
+			}
+		}
+	})
+
+	_ = engine.Run(":8081")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module notify-center
 
 go 1.12
+
+require (
+	github.com/emirpasic/gods v1.12.0 // indirect
+	github.com/gin-gonic/gin v1.4.0 // indirect
+	github.com/gorilla/websocket v1.4.0 // indirect
+)
