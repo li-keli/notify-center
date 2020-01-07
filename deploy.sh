06e31c17aaaf1e27c4a8d1d576941859b51a6c31
李科笠 <13120168539@163.com>
2020-01-07 17:08:53 +0800

    fix 准备上线

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo "[*] 开始构造comet包"
-go build -mod=vendor -ldflags="-s -w" -installsuffix cgo -o ./comet/app ./comet
-echo "[*] 开始构造server包"
-go build -mod=vendor -ldflags="-s -w" -installsuffix cgo -o ./server/app ./server
-
-echo "[*] 构造完成"
+echo "[$(date +%T)] 开始编译"
+GOOS=linux CGO_ENABLED=0 go build -mod=vendor -ldflags="-s -w" -installsuffix cgo -o ./comet/app ./comet && echo "[$(date +%T)] comet编译成功" &
+GOOS=linux CGO_ENABLED=0 go build -mod=vendor -ldflags="-s -w" -installsuffix cgo -o ./server/app ./server && echo "[$(date +%T)] server编译成功" &
+wait
+echo "[$(date +%T)] 任务完成"
