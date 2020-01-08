73f9c9086765ff1271277c372b3194ad36b3199a
李科笠 <13120168539@163.com>
2020-01-08 10:32:50 +0800

    fix 准备上线

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -17,7 +17,7 @@ func NewRedisConn() {
 		DB:       3,
 	})
 	if _, e := client.Ping().Result(); e != nil {
-		logrus.Fatal("Redis连接失败", e)
+		logrus.Fatal("Redis连接失败: ", e.Error())
 	}
 	logrus.Info("Redis连接成功...")
 }
