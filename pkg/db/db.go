add13a31c5fb72aba39f5f5ea4fb6e6d7a32a3c6
李科笠 <13120168539@163.com>
2020-01-10 14:13:05 +0800

    fix

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -4,12 +4,21 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/sirupsen/logrus"
+	"notify-center/pkg/constant"
 )
 
 var conn *gorm.DB
 
 func NewDB() {
-	db, err := gorm.Open("mysql", "root:123@tcp(172.16.2.161:3300)/notify?charset=utf8&parseTime=True&loc=Local")
+	var (
+		db  *gorm.DB
+		err error
+	)
+	if constant.ProductionMode {
+		db, err = gorm.Open("mysql", "notify:rYHk7o9%(@tcp(172.16.3.33:3306)/notify?charset=utf8&parseTime=True&loc=Local")
+	} else {
+		db, err = gorm.Open("mysql", "root:123@tcp(172.16.2.161:3300)/notify?charset=utf8&parseTime=True&loc=Local")
+	}
 	if err != nil {
 		logrus.Fatalln("数据库连接失败", err)
 	}
