03cfc0cc45e05013f1b68a4bcfb9d00abfe3c366
李科笠 <13120168539@163.com>
2020-01-11 10:21:04 +0800

    fix

--- server/api/v1/vo/vo_register_terminal.go
+++ server/api/v1/vo/vo_register_terminal.go
@@ -3,15 +3,15 @@ package vo
 import "notify-center/pkg/constant"
 
 type RegisterTerminalInputVo struct {
-	JsjUniqueId  int                   `json:"JsjUniqueId" binding:"requer"`
-	PushToken    string                `json:"PushToken" binding:"min=1"`
+	JsjUniqueId  int                   `json:"JsjUniqueId" binding:"min=1"`
+	PushToken    string                `json:"PushToken" binding:"required"`
 	PlatformType constant.PlatformType `json:"PlatformType"`
 	TargetType   constant.TargetType   `json:"TargetType"`
 }
 
 type UnRegisterTerminalInputVo struct {
-	JsjUniqueId  int                   `json:"JsjUniqueId"`
-	PushToken    string                `json:"PushToken"`
+	JsjUniqueId  int                   `json:"JsjUniqueId" binding:"min=1"`
+	PushToken    string                `json:"PushToken" binding:"required"`
 	PlatformType constant.PlatformType `json:"PlatformType"`
 	TargetType   constant.TargetType   `json:"TargetType"`
 }
